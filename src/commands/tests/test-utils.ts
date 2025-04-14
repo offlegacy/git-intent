a60cacd135cb5d9e3228a4d67706d4eedf055243
Juhyeok Kang <kangju2000@gmail.com>
2025-04-14 17:51:41 +0900

    chore: update dependencies and optimize storage migration (#3)

--- src/commands/tests/test-utils.ts
+++ src/commands/tests/test-utils.ts
@@ -1,12 +1,16 @@
 import { join } from 'node:path';
-import execa, { Options } from 'execa';
+import execa, { type Options } from 'execa';
 import { testEnvironment } from './test-environment';
 
 export const CLI_PATH = join(__dirname, '../../../dist/index.js');
 
 export async function runCLI(command: string, options?: Options): Promise<execa.ExecaChildProcess>;
 export async function runCLI(command: string, args: string[], options?: Options): Promise<execa.ExecaChildProcess>;
-export async function runCLI(command: string, argsOrOptions?: string[] | Options, options?: Options): Promise<execa.ExecaChildProcess> {
+export async function runCLI(
+  command: string,
+  argsOrOptions?: string[] | Options,
+  options?: Options
+): Promise<execa.ExecaChildProcess> {
   const isArrayArgs = Array.isArray(argsOrOptions);
   const execaOptions = isArrayArgs ? options : argsOrOptions;
   const args = isArrayArgs ? argsOrOptions : [];
